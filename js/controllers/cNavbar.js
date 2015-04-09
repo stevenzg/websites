3085644f9777664c129f8ee81f3c3c0ff82099e4
GYF1 <guoyinfeng@live.com>
2015-04-09 09:04:24 +0800

    backup

--- js/controllers/cNavbar.js
+++ js/controllers/cNavbar.js
@@ -7,6 +7,15 @@
       return viewLocation === $location.path();
     };
 
+    $scope.isReading = function () {
+      var currentPage = $location.path();
+      var booResult = false;
+      if (currentPage === '/articles' || currentPage === '/books' || currentPage === '/tutorials' ) {
+        booResult = true;
+      }
+      return booResult;
+    };
+
     $scope.initialization = function () {
 
     };
