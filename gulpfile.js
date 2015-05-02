805d118ac863a910f8de7ec136a4c5eff404066b
GYF1 <guoyinfeng@live.com>
2015-05-02 11:21:23 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,36 @@
 var gulp = require('gulp'),
+  sass = require('gulp-ruby-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  minifycss = require('gulp-minify-css'),
+  jshint = require('gulp-jshint'),
+  uglify = require('gulp-uglify'),
+  imagemin = require('gulp-imagemin'),
+  rename = require('gulp-rename'),
+  concat = require('gulp-concat'),
+  notify = require('gulp-notify'),
+  cache = require('gulp-cache'),
   livereload = require('gulp-livereload'),
-  embedlr = require("gulp-embedlr");
+  del = require('del');
 
+//gulp.task('styles', function() {
+//  return gulp.src('src/styles/main.scss')
+//    .pipe(sass({ style: 'expanded' }))
+//    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+//    .pipe(gulp.dest('dist/assets/css'))
+//    .pipe(rename({suffix: '.min'}))
+//    .pipe(minifycss())
+//    .pipe(gulp.dest('dist/assets/css'))
+//    .pipe(notify({ message: 'Styles task complete' }));
+//});
 
-gulp.src("*.html")
-  .pipe(embedlr())
-  .pipe(gulp.dest("./dist"));
-
-gulp.task('watch', function() {
-
-  // Create LiveReload server
-  livereload.listen();
-
-  // Watch any files in dist/, reload on change
-  gulp.watch(['js/**/*.*']).on('change', livereload.changed);
-  //gulp.watch('js/**/*.*');
-
-
-  //var server = livereload();
-
-  // app/**/*.*的意思是 app文件夹下的 任何文件夹 的 任何文件
-  //gulp.watch('js/**/*.*', function (file) {
-  //  server.changed(file.path);
-  //});
+gulp.task('scripts', function() {
+  return gulp.src('js/**/*.js')//src/scripts/**/*.js
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
 });
